57df4f64862aad38f9957b8c94dc439712f61f86
Dennis Renz <dennis@r-renz.de>
2023-07-17 10:41:34 +0200

    Print ulong max

--- src/RainForestComplexity/Program.cs
+++ src/RainForestComplexity/Program.cs
@@ -1,8 +1,8 @@
-﻿using System.Globalization;
+using System.Globalization;
 using RainForestComplexity;
 
-const int r = 10;
-const int d = 10;
+const int r = 16;
+const int d = 16;
 const int m = 1;
 
 var innerLoop = Complexity.InnerLoop(r, d, m);
@@ -13,6 +13,11 @@ var outerLoop = Complexity.OuterLoop(r, d);
 var outerLoopNumberString = outerLoop.ToString("N0", CultureInfo.InvariantCulture);
 var outerLoopExponentialString = outerLoop.ToString("E2", CultureInfo.InvariantCulture);
 
+var ulongMax = ulong.MaxValue;
+var ulongMaxString = ulongMax.ToString("N0", CultureInfo.InvariantCulture);
+var ulongMaxExponentialString = ulongMax.ToString("E2", CultureInfo.InvariantCulture);
+
 Console.WriteLine($"r = {r}, d = {d}, m = {m}");
+Console.WriteLine($"{nameof(ulongMax)} = {ulongMaxString} = {ulongMaxExponentialString}");
 Console.WriteLine($"{nameof(Complexity.InnerLoop)} = {innerLoopNumberString} = {innerLoopExponentialString}");
 Console.WriteLine($"{nameof(Complexity.OuterLoop)} = {outerLoopNumberString} = {outerLoopExponentialString}");
